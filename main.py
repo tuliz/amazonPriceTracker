68d11067f2bb3a3b024ed86963db423a15e4cb1f
yarden <yarden.tulchinsky@gmail.com>
2024-06-25 22:44:04 +0300

    adding products links and target price to csv to manage them there and get the data with pandas. check if the price of the product is smaller then the target price, if yes send an email with smtplib with the product price,title and url

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
-URL = 'https://www.amazon.com/-/he/dp/B0CL61F39H/ref=sr_1_1?crid=3L6GM9V7VEPQQ&dib=eyJ2IjoiMSJ9.AHjTd7ZFe8s8RHzX7QExwDesCneAdSrHHQ5Jj2D66rvF-pjGi8aIBEUBd2Es1eHM3WTiK9PkEloVhyt-Gwo_VaVSD8HWlrHw6lknKp9XIn8oft0hEMiDGCX3gYhF_YgGtnWCzQsgpDxSi3BgTP6xW0HpaqLX4Dsw1vV25Gvj4FPC8qXXkz44FlsvvxvaSa6AFy5wkHuNWqZHqfeT36vbd69SHq-P4RBWnvE2RnyMGLI.ieKx5HshoxJ3fcASVB0-MiAfluQT7N2YJ562ko-1apU&dib_tag=se&keywords=playstation%2B5&qid=1719339837&sprefix=plays%2Caps%2C335&sr=8-1&th=1'
+import pandas
+import smtplib
+TARGET_PRICE = 450
+USER = ''
+PASSWORD = ''
+EMAIL = ''
+
+products_dataframe = pandas.read_csv('products.csv')
+products_dict = products_dataframe.to_dict(orient='records')
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 ',
     'Accept-Language': 'he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6'
 }
 
-response = requests.get(URL,headers=headers)
-response.raise_for_status()
-data = response.text
+for product in products_dict:
 
-soup = BeautifulSoup(data, 'html.parser')
-product_price = float(soup.find(name='span', class_='a-price').getText().split('$')[1].split('\u200e')[1])
+    response = requests.get(product['product_url'],headers=headers)
+    response.raise_for_status()
+    data = response.text
+
+    soup = BeautifulSoup(data, 'html.parser')
+    product_title = soup.find(name='span', id='productTitle').getText()
+    product_price = float(soup.find(name='span', class_='a-price').getText().split('$')[1].split('\u200e')[1])
+    product_target_price = product['target_price']
+    product_url = product['product_url']
+    if product_price < product_target_price:
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(USER,PASSWORD)
+            connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL, msg=f'the product {product_title} is now at a price of ${product_target_price}\n{product_url}')
